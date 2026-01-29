f0f440999dc84dd24cb1dcc7e296d90a7481d372
Ariel Martinez <arielstereo@msn.com>
2026-01-29 17:39:25 -0300

    feat: adjust margins in Brand component, update Header component styles, and reposition ScrollToTop button

--- src/app/components/Brand.jsx
+++ src/app/components/Brand.jsx
@@ -1,6 +1,6 @@
 const Brand = () => {
   return (
-    <div className="bg-pink-600 text-white flex justify-center items-center text-base font-semibold py-2 mt-16 md:mt-20 overflow-hidden">
+    <div className="bg-pink-600 text-white flex justify-center items-center text-base font-semibold py-2 mt-20 overflow-hidden">
       <p className="marquee text-sm">
         Envios sin cargo en CABA en pedidos mayores a $100.000 &nbsp; | &nbsp;
         No tenemos un mínimo de compra &nbsp;| &nbsp; Arma tu kit personalizable
